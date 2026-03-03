1419fd424ed7d9d682f157390e554665898ae705
schausse-hash <schausse@gmail.com>
2026-03-02 23:33:15 -0500

    Update sb-client.js

--- assets/js/sb-client.js
+++ assets/js/sb-client.js
@@ -1,1 +1,8 @@
+// === SUPABASE CLIENT CENTRALISÉ ===
 
+// ⚠️ Mets ici TES vraies valeurs
+const SUPABASE_URL = "https://xgyskiatppgaeaamjhxr.supabase.co";
+const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InhneXNraWF0cHBnYWVhYW1qaHhyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzA0OTgxNTQsImV4cCI6MjA4NjA3NDE1NH0.67KCcUWlJij-scDoCUvZpkiCle5-mHVmy-inRk96Tac";
+
+// Création du client
+window.sb = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
